46147769a65421db943148809f80a9f105341311
Praneetha Charugundla <88439139+PraneethaCharugundla@users.noreply.github.com>
2022-10-03 12:07:48 +0530

    DOCS : updated indentation

--- Hypotenuse/hypotenuse.js
+++ Hypotenuse/hypotenuse.js
@@ -3,18 +3,17 @@ const hypotenuseButton = document.querySelector("#hypotenuse-btn");
 const outputE1 = document.querySelector("#output");
 
 
-function caluclateSumOfSquares (a,b){
-    const sumOfSquares = a*a + b*b ;
-   // console.log(sumOfSquares);
-   return sumOfSquares;
+function caluclateSumOfSquares(a, b) {
+    const sumOfSquares = a * a + b * b;
+    // console.log(sumOfSquares);
+    return sumOfSquares;
 }
 
 
-function caluclateHypotenuse(){
-const sumOfSquares = caluclateSumOfSquares
-(Number(sides[0].value),Number(sides[1].value));
-const lengthOfHypotenuse = Math.sqrt(sumOfSquares);
-outputE1.innerText = "The length of hypotenuse is" + lengthOfHypotenuse.toFixed(2);
+function caluclateHypotenuse() {
+    const sumOfSquares = caluclateSumOfSquares(Number(sides[0].value), Number(sides[1].value));
+    const lengthOfHypotenuse = Math.sqrt(sumOfSquares);
+    outputE1.innerText = "The length of hypotenuse is " + lengthOfHypotenuse.toFixed(2);
 }
 
-hypotenuseButton.addEventListener('click',caluclateHypotenuse);
+hypotenuseButton.addEventListener('click', caluclateHypotenuse);
